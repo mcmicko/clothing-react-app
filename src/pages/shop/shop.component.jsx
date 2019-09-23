4b18ca16f8f734d119c57afc682ddfd600f00dff
mcmickoo <mcmzrc@gmail.com>
2019-09-23 18:18:07 +0200

    collection

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,23 +1,16 @@
 import React, { Component } from 'react'
-import SHOP_DATA from './shop.data'
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 
 export class ShopPage extends Component {
-  state = {
-    collections: SHOP_DATA
-  }
   render() {
     return (
       <div className="shop-page">
-        {
-          this.state.collections.map(({id, ...otherCollectionProps}) => (
-            <CollectionPreview key={id} {...otherCollectionProps}/>
-          ))
-        }
+        <CollectionsOverview />
       </div>
     )
   }
 }
+
 
 export default ShopPage
